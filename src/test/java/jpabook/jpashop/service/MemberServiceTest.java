0ed512fcaa076db7e0e341825701ca716c18a1b5
onepunchkim <hyukjin4711@gmail.com>
2025-07-26 19:40:14 +0900

    member domain

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,7 +1,19 @@
 package jpabook.jpashop.service;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MemberServiceTest {
+
+    @Test
+    public void 회원가입() throws Exception {
+
+        //Given
+
+        //When
+
+        //Then
+    }
 
 }
