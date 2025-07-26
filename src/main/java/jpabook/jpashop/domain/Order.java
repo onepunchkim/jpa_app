e353297f04c7d87dfe2280e69a2688581e1eed67
onepunchkim <hyukjin4711@gmail.com>
2025-07-26 18:10:27 +0900

    add entity

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,7 +19,7 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY) //FetchType default 값이 EAGER 이므로 바꿔줘야 함 
     @JoinColumn(name = "member_id")
     private Member member; //주문 회원
 
