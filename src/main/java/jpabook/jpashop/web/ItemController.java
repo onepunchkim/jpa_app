38610162e83b20f14f1d7e0a48a148e47d08c9f0
성스러운 성진이 <hyukjin4711@gmail.com>
2025-08-05 22:20:16 +0900

    merge

--- src/main/java/jpabook/jpashop/web/ItemController.java
+++ src/main/java/jpabook/jpashop/web/ItemController.java
@@ -71,17 +71,20 @@ public class ItemController {
     * 상품 수정
     * */
     @PostMapping(value = "/items/{itemId}/edit")
-    public String updateItem(@ModelAttribute("form") BookForm form) {
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
 
-        Book book = new Book();
+/*        Book book = new Book();
         book.setId(form.getId());
         book.setName(form.getName());
         book.setPrice(form.getPrice());
         book.setStockQuantity(form.getStockQuantity());
         book.setAuthor(form.getAuthor());
         book.setIsbn(form.getIsbn());
-
         itemService.saveItem(book);
+*/
+
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
+
         return "redirect:/items";
     }
 }
