a6123de98befc591a5638a8e72ffe53e779032f5
onepunchkim <hyukjin4711@gmail.com>
2026-02-20 17:38:41 +0900

    api Lazyloading and JPA Performance Tuning

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,24 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+//		Hibernate5Module hibernate5Module = new Hibernate5Module();
+//		//강제 지연 로딩 설정, 엔티티가 직접 노출된다
+		//엔티티를 직접 노출할 때는 양방향 연관관계가 걸린 곳은 꼭! 한곳을 @JsonIgnore 처리 해야 한다. 안그러면 양쪽을 서로 호출하면서 무한 루프가 걸린다.
+//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return new Hibernate5Module();
 	}
 
 }
