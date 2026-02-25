f25cb244f90329cb42d6a607907563f9db4e5e80
onepunchkim <hyukjin4711@gmail.com>
2026-02-25 18:52:45 +0900

    api Collection JPA Performance Tuning

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -98,6 +98,16 @@ public class OrderRepository {
                 .getResultList();
     }
 
+    public List<Order> findAllWithDelivery(int offset, int limit) {
+        return em.createQuery(
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
     public List<Order> findWithItem() {
         return em.createQuery(
                 "select distinct o from Order o" +
